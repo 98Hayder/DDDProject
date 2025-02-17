11874e39c371f6384fff6684c05b09922aa90144
98Hayder <133206334+98Hayder@users.noreply.github.com>
2025-02-17 12:55:55 +0300

    hayder ali

---
+++ DDDProject.API/Middleware/JwtMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using DDDProject.Infrastructure.DbContexts;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace DDDProject.API.Middleware
+{
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public JwtMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext context, ApplicationDbContext dbContext)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (token != null)
+            {
+                var isRevoked = dbContext.RevokedTokens.Any(rt => rt.Token == token);
+                if (isRevoked)
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("تم تسجيل الخروج. لا يمكن استخدام هذا التوكن.");
+                    return;
+                }
+
+                AttachUserToContext(context, token);
+            }
+
+            await _next(context);
+        }
+
+        private void AttachUserToContext(HttpContext context, string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes(_configuration["Jwt:Key"]);
+                var parameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                };
+
+                var principal = tokenHandler.ValidateToken(token, parameters, out _);
+                context.User = principal;
+            }
+            catch
+            {
+            }
+        }
+
+    }
+}
