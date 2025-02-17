c0cdbfe30191743dc80a4f7c2ccd7c73fd40c862
98Hayder <133206334+98Hayder@users.noreply.github.com>
2025-02-17 08:50:17 +0300

    HAYDER ALI

---
+++ DDDProject.Domain/IRepositories/Log/ILogRepository.cs
@@ -0,0 +1,10 @@
+﻿using BookstoreAPI.Dtos.LogDto;
+using BookstoreAPI.Dtos;
+
+namespace BookstoreAPI.Repositories.Log
+{
+    public interface ILogRepository
+    {
+        Task<PaginatedResultDto<Entities.Logging>> GetLogAsync(LogFilterForm filter);
+    }
+}
---
+++ DDDProject.Infrastructure/Repositories/Log/LogRepository.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using BookstoreAPI.DbContexts;
+using BookstoreAPI.Dtos;
+using BookstoreAPI.Dtos.LogDto;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookstoreAPI.Repositories.Log
+{
+    public class LogRepository: ILogRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LogRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PaginatedResultDto<Entities.Logging>> GetLogAsync(LogFilterForm filter)
+        {
+            int currentPage = Math.Max(filter.PageNumber ?? 1, 1);
+            int currentPageSize = Math.Max(filter.PageSize ?? 10, 1);
+            var query = _context.Logs.AsQueryable();
+
+            int totalRecords = await query.CountAsync();
+            int totalPages = (int)Math.Ceiling(totalRecords / (double)currentPageSize);
+
+            var log = await query
+                     .OrderByDescending(c => c.Id)
+                    .Skip((currentPage - 1) * currentPageSize)
+                    .Take(currentPageSize)
+                    .ToListAsync();
+
+
+            return new PaginatedResultDto<Entities.Logging>
+            {
+                TotalRecords = totalRecords,
+                TotalPages = totalPages,
+                PageNumber = currentPage,
+                PageSize = currentPageSize,
+                Data = log,
+            };
+
+        }
+
+    }
+}
